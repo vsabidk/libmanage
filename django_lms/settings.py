b68fa1067cca54e5ff22391aae02054dc150902b
vsabidk <134827312+vsabidk@users.noreply.github.com>
2023-06-14 16:41:37 +0530

    Update settings.py

--- django_lms/settings.py
+++ django_lms/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-j7xv%)66l8u0^8sxnj7()z$zll8&a2*_h2r84yeo9f2fidx8^7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
+
 
 
 # Application definition
